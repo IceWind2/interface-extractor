53a4963d51ce16dfea183594c440a5ef1abc32ce
Anton Glazyrin <a.glazyrin@spbu.ru>
2021-09-10 09:12:40 +0300

    generation added

--- task1/interface-extractor/src/main/kotlin/com/test_task/InterfaceGeneration.kt
+++ task1/interface-extractor/src/main/kotlin/com/test_task/InterfaceGeneration.kt
@@ -1,5 +1,8 @@
 package com.test_task
 
+import com.github.javaparser.ast.CompilationUnit
+import com.github.javaparser.ast.Modifier
+import java.io.File
 import java.nio.file.Paths
 
 internal abstract class InterfaceGenerator(config: Config) {
@@ -10,6 +13,24 @@ internal abstract class InterfaceGenerator(config: Config) {
 
 internal class JavaInterfaceGenerator(config: Config) : InterfaceGenerator(config) {
 
+    private fun createCode(info: InterfaceInfo, name: String) : String {
+        val cu = CompilationUnit()
+        val newInterface = cu.addInterface(name).setPublic(true)
+
+        info.methods.forEach {method ->
+            val newMethod = newInterface.addMethod(method.sign.name)
+            method.modifiers.forEach{
+                newMethod.addModifier(Modifier.Keyword.valueOf(it.uppercase()))
+            }
+            method.params.forEach {
+                newMethod.addParameter(it.type, it.name)
+            }
+            newMethod.setType(method.sign.type)
+        }
+
+        return newInterface.toString()
+    }
+
     override fun generateInterface(info: InterfaceInfo?) {
         if (info == null) {
             println("Generation error")
@@ -19,6 +40,8 @@ internal class JavaInterfaceGenerator(config: Config) : InterfaceGenerator(confi
         val name = _config.interfaceName ?: info.name + "Interface"
         val path = _config.exportPath ?: "${Paths.get(info.path).parent}\\$name.java"
 
+        val code = createCode(info, name)
+        File(path).writeText(code);
         println("Created $path")
     }
 }
